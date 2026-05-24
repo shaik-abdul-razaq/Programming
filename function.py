5886e1ed3989087f336764f431c5806120ea984d
Abdul <shaikrazak463@gmail.com>
2026-05-24 23:14:32 +0530

    added new python files

---
+++ function.py
@@ -0,0 +1,15 @@
+# def add():
+#     a=10
+#     b=15
+#     print(a+b)
+# add()
+# Sub
+# def sub(a,b):
+#      print(a-b)
+# sub(20,20)
+# def add(a,b):
+#     return a+b
+# a=int(input("enter the a value"))
+# b=int(input("enter the b value"))
+# result=add(a,b)
+# print(result)
